c4a4f7f09cc86d6a90f0fdac9ccd1a83c5de28a3
Abdallah <bratosab@gmail.com>
2022-06-19 14:26:31 +0200

    feat(event loop): Added more examples for micro task queue explanation

--- event-loop.js
+++ event-loop.js
@@ -5,3 +5,24 @@ function eventLoop() {
 
   console.log('console.log', 'JavaScript ROCKS!?!');
 }
+
+async function eventLoop2() {
+  setTimeout(() => console.log('setTimeout', "I'm FIRST!"), 0);
+
+  const promise = new Promise((resolve, reject) => {
+    console.log('Inside Promise')
+    resolve('test')
+  })
+  const val = await promise
+  console.log('Promise await', 'Am I second????')
+
+  console.log('console.log', 'JavaScript ROCKS!?!');
+
+  return val
+}
+
+async function main() {
+  console.log(await eventLoop2())
+}
+
+//main()
